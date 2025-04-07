fdf5b967f71ced064f9294718a27f3742a357fdb
Renan <gohautrenan@gmail.com>
2025-04-07 13:58:24 +0200

    keyclock

--- src/main/java/fr/rgohaut/ecocollect/configuration/SecurityConfig.java
+++ src/main/java/fr/rgohaut/ecocollect/configuration/SecurityConfig.java
@@ -1,45 +1,29 @@
 package fr.rgohaut.ecocollect.configuration;
 
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) {
-        KeycloakAuthenticationProvider provider = new KeycloakAuthenticationProvider();
-        provider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-        auth.authenticationProvider(provider);
-    }
-
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> {})
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/collectes/**").hasRole("COLLECTOR")
-                        .requestMatchers("/api/poubelles/**").authenticated()
+                .authorizeHttpRequests(authz -> authz
+                       // .requestMatchers("/swagger-ui/**").permitAll()
+                       // .requestMatchers("/api/public/**").permitAll()
+                        //.requestMatchers("/admin/**").hasRole("admin")
+                      //  .requestMatchers("/user/**").hasRole("user")
                         .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri("http://localhost:8082/realms/EcoCollect/protocol/openid-connect/certs")
+                        )
                 );
         return http.build();
     }
